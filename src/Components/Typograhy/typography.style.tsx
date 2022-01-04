20b8193c36b3c406e3b6b7d65d8ccbacb47a6c93
lubnaabd <lubna_mosa@hotmail.com>
2022-01-04 21:55:10 +0200

    edit condiation

--- src/Components/Typograhy/typography.style.tsx
+++ src/Components/Typograhy/typography.style.tsx
@@ -3,10 +3,10 @@ import { IPropsTypographyStyle } from './types'
 
 export const Container = styled.div<IPropsTypographyStyle>`
 ${({ color, margin, fontSize, weight, padding ,lineHeight}) => css<IPropsTypographyStyle>`
-  font-size: ${ typeof fontSize === 'string'?`${fontSize}px`: fontSize|| "16px"};
+  font-size: ${ typeof fontSize === 'string'?fontSize : `${fontSize}+px`|| "16px"};
   color:${color || "black"}  ;
   font-weight: ${weight|| `500`};
-  margin: ${typeof margin === 'string'?`${margin}px`: margin || `0px`};
-  padding: ${typeof padding === 'string'?`${padding}px`: padding|| `0px`};
-  line-height: ${typeof lineHeight === 'string'?`${lineHeight}px`:lineHeight|| `normal`} ;
+  margin: ${typeof margin === 'string'? margin: `${margin}px` || `0px`};
+  padding: ${typeof padding === 'string'?padding: `${padding}px`|| `0px`};
+  line-height: ${typeof lineHeight === 'string'?lineHeight:`${lineHeight}px`|| `normal`} ;
   `}`
