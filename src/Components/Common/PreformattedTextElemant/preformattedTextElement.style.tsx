b8004b8fbc29ad6279e908231b5b329b3e4e7e47
lubnaabd <lubna_mosa@hotmail.com>
2022-01-04 21:44:50 +0200

    add common folder

---
+++ src/Components/Common/PreformattedTextElemant/preformattedTextElement.style.tsx
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components';
+export const Code =styled.div`
+    background-color: rgb(0, 30, 60);
+    margin: 2rem;
+    padding: 2rem;
+    width: 500px;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align:center;
+    border-radius: 1rem;
+    border: 1px solid rgb(19, 47, 76);
+     color: rgb(252, 146, 158);
+    overflow: auto;`
