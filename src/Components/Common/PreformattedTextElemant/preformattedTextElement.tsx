80c9cc1a18a6eb3d97553120530e10e77d3bbbee
lubnaabd <lubna_mosa@hotmail.com>
2022-01-05 10:26:27 +0200

    add comment how to use preformattedTextElement

--- src/Components/Common/PreformattedTextElemant/preformattedTextElement.tsx
+++ src/Components/Common/PreformattedTextElemant/preformattedTextElement.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { Code } from './preformattedTextElement.style';
 
+//when use preformattedTextElement example:
+
+/* const textElment: string = `
+  import * as React from 'react'; 
+  import { styled } from '@mui/material/styles';
+  const Div = styled('div')(({ theme }) => ({
+   ...theme.typography.button,
+   backgroundColor: theme.palette.background.paper,
+   padding: theme.spacing(1),
+ }));
+ export default function TypographyTheme() {
+ return &ltDiv&gt{"This div's text looks like that of a button."}&lt/Div&gt; `
+
+
+ 
+ <PreformattedTextElement textElement={textElment} /> */
+
 interface IProps {
     textElement: string
 }
@@ -11,7 +28,7 @@ const preformattedTextElement = (props: IProps) => {
             <Code>
                 <pre>
                     <code>
-                    <div dangerouslySetInnerHTML={{ __html: props.textElement }} /></code>
+                        <div dangerouslySetInnerHTML={{ __html: props.textElement }} /></code>
                 </pre>
             </Code>
         </div>
