d9f8acf500abc9b55de5c4dce911d9a8424f281e
ferassalhelo <feras.s.alhelo@gmail.com>
2022-01-06 10:29:06 +0200

    headerComponet, searchComponent, notFoundPage

---
+++ src/Components/Header/HeaderBox.style.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const HeaderBox = styled.div`
+  border-bottom: 1px solid silver;
+  display: flex;
+  padding: 10px 150px;
+
+  @media (max-width: 992px) {
+    & {
+      padding: 10px 125px;
+    }
+  }
+  @media (max-width: 768px) {
+    & {
+      padding: 10px 50px;
+    }
+  }
+
+  @media (max-width: 556px) {
+    & {
+      padding: 8px 20px;
+    }
+  }
+`;
+
+export default HeaderBox;
