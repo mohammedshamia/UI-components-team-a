1108eec9bf8c9e3e17cc5da783b54d28ee8ca610
AhmadSaleh22 <dev.ahmadsaleh@gmail.com>
2022-01-06 22:37:05 +0200

    adding app main to test components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import AvatarGroup from './Components/AvatarGroup/AvatarGroup';
+import { AvatarProps } from './@types/AvatarTypes/AvatarProps';
+import { Avatar } from './Components/Avatar/Avatar.style';
+import {SingleAvatar} from './Components/Avatar/Avatar';
+import ImageA from './aa.jpg';
 function App() {
   return (
-    <div className="App">
-        Hello, It's UI Component Project
-    </div>
+    <>
+    <SingleAvatar kind='circular' avatar={<img src={ImageA} alt='lkf' />} hidden= {true} src={ImageA} />
+      <AvatarGroup kind={'circular'} avatars={['','','','']} displayAllOnHover={true} max={1} size={60} />
+    <AvatarGroup displayAllOnHover={true} avatars={['ah m','Ahmad Saleh','a o']} kind='circular' max={1} size={60} uppercase={true} />
+    </>
   );
 }
 
