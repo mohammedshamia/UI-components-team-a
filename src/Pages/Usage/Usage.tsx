482bae157eec79b56348c19dc9b112597c08a906
lubnaabd <lubna_mosa@hotmail.com>
2022-01-06 22:19:23 +0200

    create page usage

---
+++ src/Pages/Usage/Usage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '../../Components/Typograhy/typography';
+
+const Usage = () => {
+    return (
+        <>
+            <Typography variant='h1'>
+                Usage
+            </Typography>
+            <Typography >
+                Get started with React and UI in no time.
+            </Typography></>
+    );
+}
+
+export default Usage;
