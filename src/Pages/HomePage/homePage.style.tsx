f723ba0df696c56cb0090bd30522f66dfd36c38d
ferassalhelo <feras.s.alhelo@gmail.com>
2022-01-08 02:25:59 +0200

    Do style Review for home page

--- src/Pages/HomePage/homePage.style.tsx
+++ src/Pages/HomePage/homePage.style.tsx
@@ -1,59 +1,75 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Wrapper = styled.div`
-    display: flex;
-    flex-wrap: nowrap;
-    width: 90%;
-    margin : 50px 100px;
-    flex-direction: row;
-    text-align:center;
-    align-items: center;
-    justify-content : space-between;
-`
+  display: flex;
+  flex-wrap: nowrap;
+  padding: 50px 0 50px 150px;
+  flex-direction: row;
+  text-align: center;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 860px) {
+    padding: 50px 70px;
+    width: auto;
+    display: block;
+    text-align: center;
+  }
+  @media (max-width: 360px) {
+    padding: 30px 10px;
+  }
+`;
 export const ContentLeft = styled.div`
-text-align: left;
-width : 50%;
-max-width:60%;
-justify-content: center;
-margin:1rem
+  text-align: left;
+  width: 40%;
+  justify-content: center;
+
+  padding-right: 10%;
+
+  @media (max-width: 860px) {
+    padding-right: 0px;
+
+    width: 100%;
+    text-align: center;
+    max-width: 100%;
+  }
 `;
 
 export const ContentRight = styled.div`
-
-background-color: #F3F6F9;
-width : 50%;
-max-width:100%;
-display: flex;
-flex-wrap: wrap;
-flex-direction: row;
-align-content: space-between;
-padding:2rem;
-border-radius: 10px;
-@media (max-width: 768px) {
+  background-color: #f3f6f9;
+  width: 50%;
+  max-width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-content: space-between;
+  padding: 2rem;
+  border-radius: 10px;
+  @media (max-width: 860px) {
     display: none;
-}
+  }
 `;
 
 export const Button = styled(Link)`
-display: flex;
-box-shadow: none;
-background-color: #007FFF;
-color: #fff;
-padding: 1.1rem 1rem;
-align-items: center;
-list-style: none;
-width:25%;
-text-decoration: none;
-font-size: 16px;
-border-radius: .6rem; 
-@media (max-width: 768px) {
-    width:40%;
-}
-&:hover {
-  background:#5468ff;
-}
-`
-export const Content= styled.div`
-margin-left:1.8rem;
-`
+  display: block;
+  box-shadow: none;
+  background-color: #007fff;
+  color: #fff;
+  padding: 1.1rem 0;
+  width: 190px;
+  margin: 40px 0;
+  text-decoration: none;
+  font-size: 17px;
+  border-radius: 0.6rem;
+  font-weight: 700;
+  text-align: center;
+  &:hover {
+    background: #0059b2;
+  }
+  @media (max-width: 860px) {
+    width: 100%;
+  }
+`;
+export const Content = styled.div`
+  padding-left: 1.8rem;
+`;
