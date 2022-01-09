ce3f81a0a6ad168fe5d99230e36923d8c0852cad
reeham_raed <rolla.ghanima@gmail.com>
2022-01-09 17:38:54 +0200

    Container Ref to close dialog

--- src/Components/Dialog/Dialog/index.tsx
+++ src/Components/Dialog/Dialog/index.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import {Wrapper, Container } from './Wrapper.style'
+import {useEffect, useRef} from "react";
 interface DialogProps {
     children? : React.ReactNode;
     open:boolean;
-    onClose?:()=> void;
+    setIsOpen:(value: boolean | ((value:boolean) => boolean)) => void;
 }
-const Dialog:React.FC <DialogProps> = ({children, open, onClose, ...props})=> {
-    console.log(open, 'open');
+const Dialog:React.FC <DialogProps> = ({children, open,setIsOpen,  ...props})=> {
+    const wrapperRef = useRef(null);
+    const closeByRef = (e:React.MouseEvent) => {
+        if (wrapperRef.current === e.target) {
+            setIsOpen(()=> false);
+        }
+    };
     return (
         <>
         {open&&
-        <Container>
+        <Container ref={wrapperRef} onClick={closeByRef}>
             <Wrapper>
                 {children}
             </Wrapper>
